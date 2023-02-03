e27ea6366821a1dffd49c8874e6c0e04b43cc527
luxinru <865666015@qq.com>
2023-02-03 17:20:42 +0800

    fixed bug

--- src/views/home.vue
+++ src/views/home.vue
@@ -84,7 +84,81 @@
       2015年6月，李克强总理在法国访问期间宣布了我国二氧化碳排放将在2030年左右达到峰值，为未来“碳中和”目标跨出了阶段性的步伐。2020年9月，习近平总书记提出力争于2030年前“碳达峰”，2060年前实现“碳中和”，这是我国首次推出“碳中和”。2021年3月，《2021年政府工作报告》提出扎实做好碳达峰、碳中和各项工作，意味着中国已正式将“碳中和”理念纳入顶层布局。
     </span>
 
-    <img class="architecture" src="" alt="" />
+    <img class="architecture" src="@/assets/images/后端.png" alt="" />
+
+    <section class="inside_story">
+      <div class="left">
+        <span>了解内情</span>
+        <p>
+          查看我们的基准白皮书、文章和更多资源，帮助您作为投资者保持领先地位
+        </p>
+      </div>
+
+      <div class="right">
+        <div class="item">积极扩大有效投资！</div>
+        <div class="item">瑟瑟发抖！失业率逼近两成，谁来拯救这届年轻人？</div>
+        <div class="item">瑟瑟发抖！失业率逼近两成，谁来拯救这届年轻人？</div>
+        <div class="item">瑟瑟发抖！失业率逼近两成，谁来拯救这届年轻人？</div>
+        <div class="item">瑟瑟发抖！失业率逼近两成，谁来拯救这届年轻人？</div>
+        <div class="item">瑟瑟发抖！失业率逼近两成，谁来拯救这届年轻人？</div>
+        <div class="item">瑟瑟发抖！失业率逼近两成，谁来拯救这届年轻人？</div>
+        <div class="item">瑟瑟发抖！失业率逼近两成，谁来拯救这届年轻人？</div>
+        <div class="item">瑟瑟发抖！失业率逼近两成，谁来拯救这届年轻人？</div>
+        <div class="item">瑟瑟发抖！失业率逼近两成，谁来拯救这届年轻人？</div>
+        <div class="item">瑟瑟发抖！失业率逼近两成，谁来拯救这届年轻人？</div>
+        <div class="item">瑟瑟发抖！失业率逼近两成，谁来拯救这届年轻人？</div>
+        <div class="item">瑟瑟发抖！失业率逼近两成，谁来拯救这届年轻人？</div>
+      </div>
+    </section>
+
+    <section class="graphic1">
+      <img src="@/assets/images/聚星logo拷贝2.png" alt="" />
+      <div class="info">
+        <span> 大鹏之动非一羽之轻，骐骥之力非一足之力 </span>
+        <p>
+          聚星团队 汇聚各界精英大咖 最专业的金融项目团队
+          聚星以超前的感知和预判互联网与实体企业发展 结合当下国内外时局
+          历经数年带领聚星每一位参与者 避坑 防雷 为每一位参与者保驾护航
+          感谢每一位参与者支持与陪伴 大爱无疆 智慧时代 品质先行 凝聚共识
+          汇聚聚星 星向未来 让我们一起在（聚星）共筑未来 杨帆起航
+          顶尖战力共聚聚星，为你披荆斩棘。
+        </p>
+      </div>
+    </section>
+
+    <section class="graphic2">
+      <div class="info">
+        <span class="title"> 为什么要加入我们日益壮大的投资者群体？ </span>
+        <div class="item mt60">
+          <p></p>
+          <span> 1-专业的项目风险评估人员 </span>
+        </div>
+        <div class="item">
+          <p></p>
+          <span> 2-专业的法务团队为项目提供详细的尽职调查与分析 </span>
+        </div>
+        <div class="item">
+          <p></p>
+          <span>
+            3-第一时间获取各行业的一手咨讯，为我们的工作与生活提供了更好的安全保证
+          </span>
+        </div>
+        <div class="item">
+          <p></p>
+          <span> 4-资金扶持.专业教学，实时直播经验分享！ </span>
+        </div>
+      </div>
+
+      <img src="@/assets/images/聚星logo拷贝3.png" alt="" />
+    </section>
+
+    <section class="carousel_block">
+      <el-carousel class="carousel" trigger="click">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3 class="small justify-center" text="2xl">{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </section>
   </div>
 </template>
 
@@ -287,6 +361,179 @@ export default {
     width: 74%;
     height: 609px;
     margin: 102px 0 156px;
+  }
+
+  .inside_story {
+    width: 100%;
+    height: 1017px;
+    background: #171d97;
+    padding: 142px 13% 94px;
+    display: flex;
+
+    .left {
+      flex: 1 0;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+
+      span {
+        font-size: 98px;
+        font-family: Source Han Sans CN;
+        font-weight: 200;
+        color: #ffffff;
+      }
+
+      p {
+        font-size: 21px;
+        font-family: Source Han Sans CN;
+        font-weight: 200;
+        color: #ffffff;
+        line-height: 29px;
+        margin-top: 26px;
+      }
+    }
+
+    .right {
+      position: relative;
+      width: 891px;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      margin-left: 75px;
+      overflow-y: auto;
+
+      &::-webkit-scrollbar {
+        display: none;
+      }
+
+      .item {
+        width: 100%;
+        height: 129px;
+        display: flex;
+        align-items: center;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        border-bottom: 1px solid #fff;
+        font-size: 21px;
+        font-family: Source Han Sans CN;
+        font-weight: 200;
+        color: #ffffff;
+        cursor: pointer;
+        padding: 0 7px;
+
+        &:first-child {
+          border-top: 1px solid #fff;
+        }
+
+        &:hover {
+          background-color: hsla(0, 0%, 100%, 0.1);
+        }
+      }
+    }
+  }
+
+  .graphic1 {
+    width: 100%;
+    height: 968px;
+    display: flex;
+    padding: 224px 13% 261px;
+    background-color: #fff;
+
+    img {
+      width: 688px;
+    }
+
+    .info {
+      flex: 1 0;
+      display: flex;
+      flex-direction: column;
+      margin-left: 159px;
+
+      span {
+        font-size: 72px;
+        font-family: Source Han Sans CN;
+        font-weight: 200;
+        color: #302f2d;
+        line-height: 88px;
+      }
+
+      p {
+        font-size: 21px;
+        font-family: Source Han Sans CN;
+        font-weight: 200;
+        color: #22211f;
+        line-height: 31px;
+        margin-top: 34px;
+      }
+    }
+  }
+
+  .graphic2 {
+    width: 100%;
+    display: flex;
+    padding: 167px 13% 119px;
+    background-color: rgba(240, 246, 252, 1);
+    display: flex;
+    align-items: center;
+
+    .info {
+      flex: 1 0;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+
+      .title {
+        font-size: 72px;
+        font-family: Source Han Sans CN;
+        font-weight: 200;
+        color: #302f2d;
+        line-height: 88px;
+      }
+
+      .item {
+        width: 100%;
+        font-size: 18px;
+        font-family: Source Han Sans CN;
+        font-weight: 200;
+        color: #22211f;
+        line-height: 55px;
+        display: flex;
+        align-items: flex-start;
+
+        p {
+          width: 8px;
+          height: 8px;
+          background: #171d97;
+          border-radius: 50%;
+          margin: 21px 21px 0 0;
+        }
+
+        span {
+          flex: 1 0;
+        }
+      }
+
+      .mt60 {
+        margin-top: 60px;
+      }
+    }
+
+    img {
+      width: 688px;
+      margin-left: 100px;
+    }
+  }
+
+  .carousel_block {
+    width: 100%;
+    padding: 211px 13%;
+    background-color: #fff;
+
+    .carousel {
+      width: 100%;
+      height: 100%;
+    }
   }
 }
 </style>
