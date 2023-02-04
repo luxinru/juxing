193917b4cef4fddb9d8ab09a357e700974eeed6f
Xinru Lu <865666015@qq.com>
2023-02-04 20:45:07 +0800

    完成时间线

--- src/views/about.vue
+++ src/views/about.vue
@@ -31,7 +31,56 @@
       </div>
     </section>
 
-    <section class="time_line"></section>
+    <section id="development_history_4" class="section">
+      <div class="main">
+        <ul>
+          <li class="pseudo">
+            <div class="year pseudo">2021</div>
+            <span class="circle pseudo"></span>
+            <div class="des">
+              聚星作为当地融资建设平台，认真履行上级赋予的职责，努力探索融资及基础设施建设模式，有力推动了经开区的建设发展。
+            </div>
+          </li>
+          <li class="pseudo">
+            <div class="year pseudo">2019</div>
+            <span class="circle pseudo"></span>
+            <div class="des">
+              采用了联邦学习的智邦平台（iBond）正式推出，由聚星自主设计、独立研发。该平台可以将数据转化成信息、模型、认知或知识，再通过联邦的方式实现数据可用，从而保证不同机构间数据“不流通”的前提下，实现“信用”和“信任”的流通，通过去中心化的方式从根本上解决数据安全和共享的问题。
+            </div>
+          </li>
+          <li class="pseudo">
+            <div class="year pseudo">2018</div>
+            <span class="circle pseudo"></span>
+            <div class="des">
+              全国社会保障基金境内委托投资管理人考评结果，聚星投研能力获得高度认可，在公司基本面单项考评中获A档，三个社保委托组合获评“综合考评A档”，此为社保考评结果的最高档。&nbsp;
+            </div>
+          </li>
+          <li class="pseudo">
+            <div class="year pseudo">2017</div>
+            <span class="circle pseudo"></span>
+            <div class="des">
+              已在香港、上海和欧洲建立业务。第一个B2B2C伙伴关系上线。与Lexington
+              Partners建立了二级合伙关系。推出首个聚星核心组合。
+            </div>
+          </li>
+          <li class="pseudo">
+            <div class="year pseudo">2016</div>
+            <span class="circle pseudo"></span>
+            <div class="des">
+              聚星克服错综复杂的外部经营环境影响，加快战略转型，搭建产业基金投资平台，进一步明晰集团化发展方向，实施业务多元化、管理精细化提升工程，开启新的发展征程。聚星顺应市场经济改革的潮流，积极培育功能性地产开发建设业务，完善产业配套，拓展孵化服务，发展园区经济。
+              &nbsp;
+            </div>
+          </li>
+          <li class="pseudo on">
+            <div class="year pseudo">2015</div>
+            <span class="circle pseudo"></span>
+            <div class="des">
+              聚星成立，凝聚了一批年轻人的力量，迈进金融投资这个领域历年来，聚星以发展经济，带动他人一起致富为己任，不断探索企业发展规律，努力做强做优做大，逐渐发展成为当地创新创业的乐土、产业投资的引擎、地产开发的先锋和承接产业转移的纽带，打造成“国内一流的专业化、市场化、多元化产业投资综合服务商”。
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
 
     <section class="vip">
       <span class="title"> 会员风采 </span>
@@ -78,6 +127,8 @@
 </template>
 
 <script>
+import $ from "jquery";
+
 export default {
   name: "AboutPage",
 
@@ -90,6 +141,45 @@ export default {
   mounted() {
     this.$nextTick(() => {
       this.init();
+
+      $("#development_history_4 li").hover(function () {
+        $("#development_history_4 li .circle::before").animate(
+          {
+            height: "118px",
+          },
+          "slow"
+        );
+      });
+
+      var timer = 3000;
+      var vv = $("#development_history_4 li");
+      var aa = $(vv).length;
+      var index = 0;
+      var addClassOn;
+      addClassOn = setInterval(function () {
+        if (index >= aa) {
+          index = 0;
+        } else {
+          $(vv).eq(index++).addClass("on").siblings().removeClass("on");
+        }
+      }, timer);
+
+      $(vv).mouseenter(function () {
+        clearInterval(addClassOn); //清除计时
+        $(vv).removeClass("on");
+        $(this).addClass("on");
+      });
+
+      $(vv).mouseleave(function () {
+        clearInterval(addClassOn); //清除计时
+        addClassOn = setInterval(function () {
+          if (index >= aa) {
+            index = 0;
+          } else {
+            $(vv).eq(index++).addClass("on").siblings().removeClass("on");
+          }
+        }, timer);
+      });
     });
   },
 
@@ -188,7 +278,7 @@ export default {
     }
   }
 
-  .time_line {
+  #development_history_4 {
     width: 100%;
     height: 510px;
     background: url("@/assets/images/5d25ec589e2eccd82824fd330d5143b43a4e6872b1f0b2-7LnDPG.png")
@@ -354,5 +444,235 @@ export default {
       }
     }
   }
+}
+
+/*! CSS Used from: http://cdn.xuansiwei.com/common/lib/bulma/0.7.5/bulma.min.css */
+li,
+ul {
+  margin: 0;
+  padding: 0;
+}
+
+ul {
+  list-style: none;
+}
+
+*,
+::after,
+::before {
+  box-sizing: inherit;
+}
+
+span {
+  font-style: inherit;
+  font-weight: inherit;
+}
+
+.section {
+  padding: 3rem 1.5rem;
+}
+
+/*! CSS Used from: http://d0000000.template.jiaogeiwo.cn/css/tailwind.css */
+*,
+::before,
+::after {
+  box-sizing: border-box;
+  border-width: 0;
+  border-style: solid;
+  border-color: #e5e7eb;
+}
+
+::before,
+::after {
+  --tw-content: "";
+}
+
+:-moz-focusring {
+  outline: auto;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+:disabled {
+  cursor: default;
+}
+
+*,
+::before,
+::after {
+  --tw-translate-x: 0;
+  --tw-translate-y: 0;
+  --tw-rotate: 0;
+  --tw-skew-x: 0;
+  --tw-skew-y: 0;
+  --tw-scale-x: 1;
+  --tw-scale-y: 1;
+  --tw-scroll-snap-strictness: proximity;
+  --tw-ring-offset-width: 0px;
+  --tw-ring-offset-color: #fff;
+  --tw-ring-color: rgb(59 130 246 / 0.5);
+  --tw-ring-offset-shadow: 0 0 #0000;
+  --tw-ring-shadow: 0 0 #0000;
+  --tw-shadow: 0 0 #0000;
+  --tw-shadow-colored: 0 0 #0000;
+}
+
+/*! CSS Used from: Embedded */
+.section {
+  padding: 3rem 1.5rem 3rem 1.5rem;
+}
+
+@media (max-width: 768px) {
+  .section {
+    padding: 2rem 1rem 2rem 1rem;
+  }
+}
+
+#development_history_4 .pseudo::before,
+#development_history_4 .pseudo::after {
+  content: "";
+  display: block;
+  position: absolute;
+}
+
+#development_history_4 ul {
+  --color: #70a4cd;
+  --border-color: #70a4cd;
+  --year-bg: #70a4cd;
+  padding: 330px 30px 50px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+}
+
+#development_history_4 ul li {
+  position: relative;
+  width: 140px;
+  height: 1px;
+}
+
+#development_history_4 ul li::before {
+  left: 10px;
+  right: 11px;
+  border-top: 1px solid var(--color);
+}
+
+#development_history_4 ul li::after {
+  left: 0;
+  top: -70px;
+}
+
+#development_history_4 ul li:last-of-type::before {
+  display: none;
+}
+
+#development_history_4 ul li .year {
+  position: absolute;
+  top: 0;
+  left: -30px;
+  width: 60px;
+  padding-top: 18px;
+  font-size: 16px;
+  color: #fff;
+  text-align: center;
+  line-height: 40px;
+}
+
+#development_history_4 ul li .year::before {
+  left: 19px;
+  top: -10px;
+  width: 21px;
+  height: 21px;
+  border: 1px solid;
+  border-color: var(--border-color);
+  border-radius: 50%;
+  transition: all 0.5s;
+}
+
+#development_history_4 ul li .year::after {
+  left: 24px;
+  top: -5px;
+  width: 11px;
+  height: 11px;
+  border-radius: 50%;
+  background: var(--year-bg);
+  transition: all 0.5s;
+}
+
+#development_history_4 ul li .circle::before {
+  left: 0;
+  bottom: 10px;
+  width: 1px;
+  height: 0;
+  background: var(--color);
+  transform: scaleY(0);
+  transform-origin: bottom;
+  transition: transform 150ms cubic-bezier(0.25, 0.74, 0.22, 0.99);
+}
+
+#development_history_4 ul li .circle::after {
+  left: 0;
+  bottom: 128px;
+  width: 7px;
+  height: 1px;
+  background: var(--color);
+  transform: scaleX(0);
+  transform-origin: left;
+  transition: transform 150ms cubic-bezier(0.25, 0.74, 0.22, 0.99);
+  transition-delay: 50ms;
+}
+
+#development_history_4 ul li .des {
+  position: absolute;
+  left: 20px;
+  bottom: 68px;
+  min-width: 300px;
+  font-size: 16px;
+  color: #fff;
+  text-align: left;
+  line-height: 26px;
+  opacity: 0;
+  pointer-events: none;
+  transform: translateX(-10px);
+  transition: opacity 125ms cubic-bezier(0.25, 0.74, 0.22, 0.99),
+    transform 425ms cubic-bezier(0.25, 0.74, 0.22, 0.99);
+}
+
+#development_history_4 ul li .des span {
+  display: block;
+  font-size: 30px;
+  font-weight: 200;
+  color: #fff;
+  line-height: 42px;
+}
+
+#development_history_4 ul li.on {
+  --border-color: #fff;
+  --year-bg: #fff;
+}
+
+#development_history_4 ul li.on .circle::before {
+  height: 118px;
+  background: #fff;
+  transform: scaleY(1);
+  transition-delay: 0s;
+}
+
+#development_history_4 ul li.on .circle::after {
+  background: #fff;
+  transform: scaleX(1);
+  transition-delay: 150ms;
+}
+
+#development_history_4 ul li.on .des {
+  opacity: 1;
+  z-index: 0;
+  transform: translateX(0);
+  transition-duration: 250ms;
+  transition-delay: 0.112s;
 }
 </style>
